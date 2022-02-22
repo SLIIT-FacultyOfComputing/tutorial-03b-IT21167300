288891813b456b3cd7061d938cee939757e09ec3
Dinal <99706572+IT21167300@users.noreply.github.com>
2022-02-22 06:13:18 +0000

    Finished all four exercises

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
+
 int main() {
+  
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
+  
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
+  
+ cout << setw(5);
+ cout << "No";
+  cout << setw(15);
+ cout << "Name";
+  cout << setw(10);
+ cout << "Marks";
+ cout << endl;
+  
  for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
+     cout << setw(5);
+     cout <<r+1;
+     cout << setw(15);
+     cout << names[r];
+     cout << setw(10);
+     cout << setprecision(2);
+     cout << setiosflags(ios::fixed);
+     cout << marks[r];
+     cout << endl;
  }
 }
--- exercise04.cpp
+++ exercise04.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 
 // Do not change the main() function
 int main() {
@@ -19,3 +19,10 @@ void print(int len, int wth) {
 }
 
 // Implement the Input Function here
+void input(int &len, int &wth)
+{
+  cout << " Enter the length: ";
+  cin >> len;
+  cout << " Enter the Width: ";
+  cin >> wth;
+}
